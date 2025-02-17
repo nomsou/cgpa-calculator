950a7fb62f3c7b665f010c56b5af2bc582afc6c6
Nomso <nomsounaogu@gmail.com>
2025-02-17 14:28:13 +0100

    modified progressbar to correct tailwind syntax

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function ProgressBar({ value }) {
+  let value_int = (value / 5) * 100;
 
-  let value_int = (value / 5) * 100
   return (
-      <CircularProgressbarWithChildren
-          value={value_int}
-          circleRatio={0.6}
-          strokeWidth={12}
-          styles={{
-              root: {
-                  transform: "rotate(-108deg)",
-              },
-              path: { stroke: "#7D52E9", strokeLinecap: "round", strokeWidth: 12 },
-              trail: { stroke: "rgba(128,128,200, 0.4)", strokeLinecap: "round",},
-          }}
-      >
-          <div style={{ fontSize: 24, marginTop: -30, textAlign:'center' }}>
-              <strong style={{display: 'block'}}>{value}</strong>
-              <small style={{fontSize: 12}}>Cumulative CGPA</small>
-          </div>
-      </CircularProgressbarWithChildren>
-  )
+    <CircularProgressbarWithChildren
+      value={value_int}
+      circleRatio={0.6}
+      strokeWidth={12}
+      styles={{
+        root: {
+          transform: "rotate(-108deg)",
+        },
+        path: { stroke: "#7D52E9", strokeLinecap: "round", strokeWidth: 12 },
+        trail: { stroke: "rgba(100,128,200, 0.4)", strokeLinecap: "round" },
+      }}
+    >
+      <div className="text-center mt-[-30px]">
+        <strong className="block text-2xl">{value}</strong>
+        <small className="text-xs">Cumulative CGPA</small>
+      </div>
+    </CircularProgressbarWithChildren>
+  );
 }
 
-export default ProgressBar
+export default ProgressBar;
